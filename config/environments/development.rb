29080e7467131051797a8911020c1a978540bfca
Caroline Visoto Nunes <carolvisoto@gmail.com>
2015-11-10 23:04:56 +0100

    Email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
     address: ENV["EMAIL_ADDRESS"],
     port: ENV["EMAIL_PORT"],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.after_initialize do
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { host: 'cognr.herokuapp.com' }
-    config.action_mailer.smtp_settings = {
-      address: ENV["EMAIL_ADDRESS"],
-      port: ENV["EMAIL_PORT"],
-      authentication: :plain,
-      user_name: ENV["EMAIL_USERNAME"],
-      password: ENV["EMAIL_PASSWORD"],
-      domain: ENV["EMAIL_DOMAIN"],
-      enable_starttls_auto: true
-    }
-  end
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'cognr.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    address: ENV["EMAIL_ADDRESS"],
+    port: ENV["EMAIL_PORT"],
+    authentication: :plain,
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"],
+    domain: ENV["EMAIL_DOMAIN"],
+    enable_starttls_auto: true
+  }
 end
