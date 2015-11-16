45503073018044221dc3507b5b0f54469d4043ca
Caroline Visoto Nunes <carolvisoto@gmail.com>
2015-11-16 19:02:02 +0100

    Removes unused files

--- app/uploaders/upload_uploader.rb
+++
@@ -1,9 +0,0 @@
-class UploadUploader < CarrierWave::Uploader::Base
-
-  storage :file
-  
-  def store_dir (filename)
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-end
