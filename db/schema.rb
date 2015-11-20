99f58e7ccaa65b00883d1423ba3aed6fbb89eecb
Caroline Visoto Nunes <carolvisoto@gmail.com>
2015-11-20 16:07:16 +0100

    add username on user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116194353) do
+ActiveRecord::Schema.define(version: 20151118204819) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151116194353) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
